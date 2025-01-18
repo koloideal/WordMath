288924814e84523cbd281e33eedac0d182d0011d
kolo <kolo.is.main@gmail.com>
2025-01-18 11:50:30 +0300

    ruff format and fix some bugs

--- func/change_lang.py
+++ func/change_lang.py
@@ -3,31 +3,31 @@ import socket
 
 
 def change_lang() -> None:
+    pc_name = socket.gethostname().strip().replace(" ", "_")
 
-    pc_name = socket.gethostname().strip().replace(' ', '_')
-
-    connection = sqlite3.connect('lang.db')
+    connection = sqlite3.connect("lang.db")
     cursor = connection.cursor()
 
     cursor.execute('''SELECT now_lang FROM lang WHERE pc_names = "%s"''' % pc_name)
     language = cursor.fetchone()
 
-    if language[0] == 'en':
-
-        cursor.execute('''UPDATE lang SET now_lang = "ru" WHERE pc_names = "%s"''' % pc_name)
+    if language[0] == "en":
+        cursor.execute(
+            '''UPDATE lang SET now_lang = "ru" WHERE pc_names = "%s"''' % pc_name
+        )
         connection.commit()
 
         cursor.close()
         connection.close()
 
-    elif language[0] == 'ru':
-
-        cursor.execute('''UPDATE lang SET now_lang = "en" WHERE pc_names = "%s"''' % pc_name)
+    elif language[0] == "ru":
+        cursor.execute(
+            '''UPDATE lang SET now_lang = "en" WHERE pc_names = "%s"''' % pc_name
+        )
         connection.commit()
 
         cursor.close()
         connection.close()
 
     else:
-
         return
--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from func.helper import helper
 
-if __name__ == '__main__':
-
+if __name__ == "__main__":
     helper()
-
