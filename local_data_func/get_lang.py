1ed3463a684f0fe61b014b3b9126109153d8bbbc
kolo <kolo.is.main@gmail.com>
2025-01-28 20:35:14 +0300

    work

--- local_data_func/get_lang.py
+++
@@ -1,9 +0,0 @@
-import json
-
-
-def get_lang() -> str:
-    with open("local_data/config.json", "r", encoding="utf-8") as file:
-        config: dict = json.load(file)
-    language: str = config["language"]
-
-    return language
--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import socket
-from typing import Callable
-from local_data_func.init_user_lang import init_user_lang
 from main_func.action_selection import action_selection
 from art import text2art
 from rich.console import Console
@@ -10,10 +7,7 @@ console = Console()
 
 
 def main():
-    pc_name = socket.gethostname().replace(" ", "_")
-    init_user_lang(pc_name)
-
-    text = text2art('WordMath', font='varsity')
+    text: str = text2art('WordMath', font='varsity')
     console.print(f'[bold blue]{text}[/bold blue]')
 
     action_selection()
