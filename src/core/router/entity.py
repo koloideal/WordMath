6ffafb297d80f7aad4a29e0d1ef985ba307f9c31
kolo <kolo.is.main@gmail.com>
2025-02-06 19:35:09 +0300

    finaaaaaaaaaaaal

--- src/core/router/entity.py
+++ src/core/router/entity.py
@@ -1,4 +1,3 @@
-from functools import wraps
 from typing import Callable, Any
 from src.core.router.exceptions import (InvalidCommandInstanceException,
                                         UnknownCommandHandlerHasAlreadyBeenCreatedException,
@@ -10,11 +9,13 @@ class Router:
                  name: str,
                  ignore_command_register: bool = False):
 
-        self.processed_commands: list[dict[str, Callable[[], None] | str]] = []
         self.ignore_command_register: bool = ignore_command_register
         self._name = name
+
+        self.processed_commands: list[dict[str, Callable[[], None] | str]] = []
         self.unknown_command_func: Callable[[str], None] | None = None
         self._is_main_router: bool = False
+
 
     def command(self, command: str, description: str) -> Callable[[Any], Any]:
         if not isinstance(command, str):
@@ -26,11 +27,11 @@ class Router:
                 self.processed_commands.append({'func': func,
                                                 'command': command,
                                                 'description': description})
-                @wraps(func)
                 def wrapper(*args, **kwargs):
                     return func(*args, **kwargs)
                 return wrapper
             return command_decorator
+
 
     def unknown_command(self, func):
         if self.unknown_command_func is not None:
@@ -38,10 +39,10 @@ class Router:
 
         self.unknown_command_func = func
 
-        @wraps(func)
         def wrapper(*args, **kwargs):
             return func(*args, **kwargs)
         return wrapper
+
 
     def input_command_handler(self, input_command):
         for command_entity in self.processed_commands:
@@ -55,11 +56,14 @@ class Router:
     def unknown_command_handler(self, unknown_command):
         self.unknown_command_func(unknown_command)
 
+
     def set_router_as_main(self):
         self._is_main_router = True
 
+
     def get_registered_commands(self) -> list[dict[str, Callable[[], None] | str]]:
         return self.processed_commands
+
 
     def get_name(self) -> str:
         return self._name
