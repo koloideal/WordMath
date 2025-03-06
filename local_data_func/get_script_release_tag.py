5c11b3fba8b71140843258c20a9d037d72340cfd
kolo <kolo.is.main@gmail.com>
2025-03-06 19:11:21 +0300

    fix

--- local_data_func/get_script_release_tag.py
+++ local_data_func/get_script_release_tag.py
@@ -1,9 +1,8 @@
 import tomllib
-from _typeshed import SupportsRead
 
 
 def get_script_tag() -> str:
-    with open("pyproject.toml", "r", encoding="utf-8") as file: # type: SupportsRead
+    with open("pyproject.toml", "rb", encoding="utf-8") as file:
         script_release_tag: str = tomllib.load(file)['project']['version']
 
     return script_release_tag
